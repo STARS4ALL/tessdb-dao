b6f0c18dbc96f383d55886f7cc29b46e1cb7b40a
Rafael Gonzalez <astrorafael@gmail.com>
2025-10-27 13:31:06 +0100

    added a __repr__ method that allows eval() later on

--- src/tessdbdao/constants.py
+++ src/tessdbdao/constants.py
@@ -7,18 +7,31 @@
 
 from enum import StrEnum
 
+
 class ObserverType(StrEnum):
     PERSON = "Individual"
     ORG = "Organization"
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.value}')"
+
+
 class PhotometerModel(StrEnum):
     TESSW = "TESS-W"
-    TESSWDL = "TESS-WDL" # Variant with datalogger
+    TESSWDL = "TESS-WDL"  # Variant with datalogger
     TESS4C = "TESS4C"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.value}')"
+
 
 class ValidState(StrEnum):
     CURRENT = "Current"
     EXPIRED = "Expired"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.value}')"
+
 
 # As returned by Nominatim search
 class PopulationCentre(StrEnum):
@@ -26,16 +39,31 @@ class PopulationCentre(StrEnum):
     MUNICIP = "municipality"
     TOWN = "town"
     CITY = "city"
-  
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.value}')"
+
+
 class TimestampSource(StrEnum):
     SUBSCRIBER = "Subscriber"
     PUBLISHER = "Publisher"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.value}')"
+
 
 class ReadingSource(StrEnum):
     DIRECT = "Direct"
     IMPORTED = "Imported"
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.value}')"
+
+
 class RegisterState(StrEnum):
     MANUAL = "Manual"
     AUTO = "Automatic"
     UNKNOWN = "Unknown"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.value}')"
